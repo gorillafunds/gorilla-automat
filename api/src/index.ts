a1188b149fb2f56794bc79c39ffdcb5e453ea0e7
smetzdev <hello@smetz.dev>
2023-02-06 18:04:45 +0100

    refactor(api): :heavy_plus_sign: add cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import { fetchStores } from "./services"
 import dotenv from "dotenv"
+import cors from "cors"
 
 dotenv.config()
 
 const app = express()
+app.use(cors())
 const PORT = 4000
 
 app.get("/get-contract-stores/:contractId", async (req, res) => {
