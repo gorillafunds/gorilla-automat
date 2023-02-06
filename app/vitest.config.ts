34fe8db391ff1d54afe18307fe2b633d909c741c
smetzdev <hello@smetz.dev>
2023-02-06 17:21:34 +0100

    refactor(app): :recycle: add ismorphic env

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -1,5 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite"
+import dotenv from "dotenv"
+
+dotenv.config({
+  path: ".env.local",
+})
 
 export default defineConfig({
   test: {
