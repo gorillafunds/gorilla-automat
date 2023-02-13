4eb5591c1a4a81d17410cafefb8113a2a58d253f
smetzdev <hello@smetz.dev>
2023-02-13 15:54:50 +0100

    fix(domain/helper): :pencil2: fix buildMetadataArray path typo

---
+++ packages/domain/src/helper/buildMetaDataArray/buildMetadataArray.test.ts
@@ -0,0 +1,31 @@
+import fs from "fs"
+import path from "path"
+import { buildMetadataArray } from "./index"
+import { testMetadataJson } from "./_testMetadataJson"
+
+const FILE_PATH = path.join(__dirname, "100073508100.svg")
+
+describe("buildMetadataArray", () => {
+  it("should return the right metadataArray for a given fileArray and configObject", async () => {
+    const svg = await fs.promises.readFile(FILE_PATH)
+    const testFile = new File([svg], "testsvg")
+    const testFileArray = [testFile]
+    const testConfig = {
+      title: "testfile",
+      description: "this is a test",
+    }
+
+    const result = buildMetadataArray(
+      testFileArray,
+      testConfig,
+      testMetadataJson,
+    )
+
+    console.log(result)
+
+    expect(result[0]).toEqual({
+      ...testConfig,
+      attributes: testMetadataJson[0].attributes,
+    })
+  })
+})
