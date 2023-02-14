ebacd9b8fbff2e15f05541cd14c7a7713a93fef7
smetzdev <hello@smetz.dev>
2023-02-14 12:12:18 +0100

    refactor(ui/screens/MintPermission.stories): :recycle: add "fakeStorage"

--- packages/ui/src/screens/MintPermission/MintPermission.stories.tsx
+++ packages/ui/src/screens/MintPermission/MintPermission.stories.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from "react"
 import { Story, Meta } from "@storybook/react"
 import { MintPermission, MintPermissionProps } from "./index"
 import { mockActions } from "../storybookMocks"
 import { StorybookAutomat } from "@gorilla-automat/domain"
 import { withMaxWidth } from "../../decorators"
+import { Spinner } from "../../components"
 
 // TODO: Add error boundary that a shop needs to be selected first
 export default {
@@ -15,8 +17,22 @@ export default {
   decorators: [withMaxWidth],
 } as Meta<MintPermissionProps>
 
-const Template: Story<MintPermissionProps> = (args) => (
-  <MintPermission {...args} />
-)
+const Template: Story<MintPermissionProps> = (args) => {
+  const [hasFakeStorage, setHasFakeStorage] = useState(false)
+
+  // Setup a fake storage on initial load
+  useEffect(() => {
+    const mockStorageObj = JSON.stringify({
+      title: "Mock Shop",
+      value: "mockShop",
+    })
+    if (!window.sessionStorage.getItem("gorillaAutomatShop"))
+      window.sessionStorage.setItem("gorillaAutomatShop", mockStorageObj)
+
+    setHasFakeStorage(true)
+  }, [])
+
+  return hasFakeStorage ? <MintPermission {...args} /> : <Spinner />
+}
 
 export const Base = Template.bind({})
