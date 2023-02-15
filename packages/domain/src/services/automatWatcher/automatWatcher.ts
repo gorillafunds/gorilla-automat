a0af6da5c4b352ac426f120dac9ad9d1c1cbef43
smetzdev <hello@smetz.dev>
2023-02-15 12:28:38 +0100

    refactor(automatWatcher): :recycle: prettier --write

--- packages/domain/src/services/automatWatcher/automatWatcher.ts
+++ packages/domain/src/services/automatWatcher/automatWatcher.ts
@@ -1,36 +1,36 @@
-import * as AWS from "aws-sdk";
-const StepFunctions = require("aws-sdk/clients/stepfunctions");
+import * as AWS from "aws-sdk"
+const StepFunctions = require("aws-sdk/clients/stepfunctions")
 
 const stepFunctions = new StepFunctions({
   region: process.env.AWS_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+})
 
 async function fetchExecution(executionArn: string) {
   const result = await stepFunctions
     .describeExecution({
       executionArn,
     })
-    .promise();
-  return result;
+    .promise()
+  return result
 }
 export async function pollExecution(executionArn: string) {
-  let execution = await fetchExecution(executionArn);
-  console.log(`Initial status: ${execution.status}`);
+  let execution = await fetchExecution(executionArn)
+  console.log(`Initial status: ${execution.status}`)
   while (
     execution.status !== "SUCCEEDED" &&
     execution.status !== "FAILED" &&
     execution.status !== "TIMED_OUT"
   ) {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    execution = await fetchExecution(executionArn);
-    console.log(`Current status: ${execution.status}`);
+    await new Promise((resolve) => setTimeout(resolve, 500))
+    execution = await fetchExecution(executionArn)
+    console.log(`Current status: ${execution.status}`)
   }
-  console.log(`Final status: ${execution.status}`);
+  console.log(`Final status: ${execution.status}`)
   if (execution.status === "SUCCEEDED") {
-    return JSON.parse(execution.output as string);
+    return JSON.parse(execution.output as string)
   } else {
-    throw new Error(`Execution failed with status: ${execution.status}`);
+    throw new Error(`Execution failed with status: ${execution.status}`)
   }
 }
