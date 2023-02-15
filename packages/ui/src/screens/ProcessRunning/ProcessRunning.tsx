77c274a5a9903c59fa489751a8689c5c1d29f6ed
smetzdev <hello@smetz.dev>
2023-02-15 11:34:03 +0100

    another fast one

--- packages/ui/src/screens/ProcessRunning/ProcessRunning.tsx
+++ packages/ui/src/screens/ProcessRunning/ProcessRunning.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState, useRef } from "react"
 import { ProgressBar } from "../../components"
 import { WizardStep, WizardStepContent } from "../../patterns"
 import { Screen } from "../types"
-import { useTimeout } from "../../helper"
 import { useStoreId } from "./_useStoreId"
 
 export type ProcessRunningProps = Screen
@@ -10,7 +9,7 @@ export type ProcessRunningProps = Screen
 export const ProcessRunning = ({ actions, automat }: ProcessRunningProps) => {
   // TODO: Try out after switching away from NextJs
   const actionRef = useRef<boolean>(false)
-  const [progress, setProgress] = useState(5)
+  const [progress] = useState(5)
   const storeId = useStoreId()
 
   // Start process on the first render
@@ -26,13 +25,6 @@ export const ProcessRunning = ({ actions, automat }: ProcessRunningProps) => {
     }
     mintAndList()
   }, [])
-
-  // Just a dummy
-  useTimeout(() => setProgress(100), 6000)
-  useTimeout(() => setProgress(75), 4500)
-  useTimeout(() => setProgress(50), 3000)
-  useTimeout(() => setProgress(25), 1500)
-  useTimeout(actions.next, 7000)
 
   return (
     <WizardStep>
