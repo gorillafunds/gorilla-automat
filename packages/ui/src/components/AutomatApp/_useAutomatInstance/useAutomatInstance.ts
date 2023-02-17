fc2a62899e830c40fd83b4b074cc2fee620a0a12
smetzdev <hello@smetz.dev>
2023-02-17 15:29:29 +0100

    refactor(ui/useAutomatInstance): :recycle: make sure instance gets only intialized once

--- packages/ui/src/components/AutomatApp/_useAutomatInstance/useAutomatInstance.ts
+++ packages/ui/src/components/AutomatApp/_useAutomatInstance/useAutomatInstance.ts
@@ -1,22 +1,24 @@
+import { useState, useEffect, useRef } from "react"
 import { IGorillaAutomat, AutomatConstructor } from "@gorilla-automat/domain"
 import { AutomatActions } from "./../types"
-
-import { useState, useEffect } from "react"
 
 export const useAutomatInstance = (
   actions: AutomatActions,
   currentState: string,
   AutomatClass: AutomatConstructor,
 ) => {
+  const actionRef = useRef(false)
   const [automat, setAutomat] = useState<IGorillaAutomat>()
 
   // Initialize instance in an async hook, because initializeWallet is async
   useEffect(() => {
+    if (actionRef.current) return
     const initializeAutomat = async () => {
       const newAutomat = new AutomatClass()
       await newAutomat.initializeWallet()
       setAutomat(newAutomat)
     }
+    actionRef.current = true
     initializeAutomat()
   }, [AutomatClass])
 
