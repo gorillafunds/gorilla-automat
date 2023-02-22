3de543ab3051d4e3037182b813c8b2164bbb0004
smetzdev <hello@smetz.dev>
2023-02-22 13:53:25 +0100

    refactor(domain/GorillaAutomat): :recycle: move {{index}} replacement to buildMetadataArray

--- packages/domain/src/helper/buildMetaDataArray/buildMetadataArray.ts
+++ packages/domain/src/helper/buildMetaDataArray/buildMetadataArray.ts
@@ -1,15 +1,23 @@
+import { replaceIndexString } from "../"
+
 export const buildMetadataArray = (
   fileArray: File[],
   config: MetadataConfig,
   metaDataJson: any,
 ) => {
-  const metaDataArray = fileArray.map((file) => {
+  const metaDataArray = fileArray.map((file, index) => {
     const { attributes } = metaDataJson.find((current: any) => {
       return file.name.includes(current.name)
     })
 
+    // Replace title and description {{index}} with the current index+1
+    const title = replaceIndexString(config.title, index + 1)
+    const description = replaceIndexString(config.description, index + 1)
+
     return {
       ...config,
+      title,
+      description,
       attributes,
     }
   })
--- packages/domain/src/lib/GorillaAutomat/GorillaAutomat.ts
+++ packages/domain/src/lib/GorillaAutomat/GorillaAutomat.ts
@@ -92,20 +92,11 @@ export class GorillaAutomat implements IGorillaAutomat {
           const account = this.wallet.activeAccount?.accountId
           const signature = await this.wallet.signMessage("hello")
 
-          // Replace title and description {{index}} with the current index+1
-          const title = config.title.replace("{{index}}", `${index + 1}`)
-          const description = config.description.replace(
-            "{{index}}",
-            `${index + 1}`,
-          )
-
           return {
             signature,
             account,
             storeId,
             minter,
-            title,
-            description,
             amount: Number(config.amount),
             ref: id?.data,
             extra: [],
