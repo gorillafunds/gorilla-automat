1c501e527c64b11e34d12f8bbcd9663bf6972f29
smetzdev <hello@smetz.dev>
2023-03-14 13:59:24 +0100

    feat(app): :sparkles: add testnet and mainnet app

--- app/src/pages/testnet.tsx
+++ app/src/pages/testnet.tsx
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Automat</title>
+        <title>Automat (TESTNET)</title>
         <meta name="description" content="Automat by GorillaFunds" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <AutomatApp AutomatClass={GorillaAutomat} isMainnet={true} />
+      <AutomatApp AutomatClass={GorillaAutomat} />
     </>
   )
 }
