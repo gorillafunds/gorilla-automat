fb8149a5199a2c4d5f9034a8d20ff21b03113970
tmtz <metzias@t-online.de>
2023-03-30 16:22:26 +0200

    feat: packages/screens StartProcess
    add getDurationTime func to calc minting-process-time

--- packages/ui/src/screens/StartProcess/StartProcess.tsx
+++ packages/ui/src/screens/StartProcess/StartProcess.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Message, Button } from "../../components"
 import {
   WizardStep,
@@ -6,16 +7,29 @@ import {
   WizardStepFooter,
 } from "../../patterns"
 import { Screen } from "../types"
+import { useSetupStorage } from "../../hooks"
 
 export type StartProcessProps = Screen
 
-export const StartProcess = ({ actions }: StartProcessProps) => {
+export const StartProcess = ({ actions, automat }: StartProcessProps) => {
+  const [count, setCount] = useState<number>(getDurationTime())
+  /**
+   *
+   * Calculate File lenght(a) of the input zip file and
+   * copys(b) (from from setup settings) for each file,
+   * to get the DurationTime for Minting => a*b*5
+   */
+  function getDurationTime() {
+    return automat.getFileCount() * useSetupStorage().amount * 3
+  }
+
   return (
     <WizardStep>
       <WizardStepHeader>Mint &amp; List</WizardStepHeader>
       <WizardStepContent className="mx-auto max-w-lg md:!py-16">
         <Message variant="important">
-          Minting and listing might take a couple of minutes.{" "}
+          Minting and listing might take
+          <span className="font-semibold"> {count}</span> minutes.{" "}
           <span className="font-semibold">
             Do not close this tab while processing!
           </span>
